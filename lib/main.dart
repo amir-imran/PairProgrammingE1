8c8f6e0ae20b6eb4ea39fe042f0314d307dee4d3
Amir Imran bin Amiruddin <amirimran@graduate.utm.my>
2021-04-26 11:45:59 +0800

    Add home page in main.dart
    Amir create something in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  ;
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      home: MyHomePage(
+        title: 'My Icon',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
